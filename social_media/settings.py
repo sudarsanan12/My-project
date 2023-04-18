45222fcff5bea39959e97abfad4c20070ffed6d5
SUDARSANAN P R <110593220+sudarsanan12@users.noreply.github.com>
2023-04-18 16:56:54 +0000

    modified:   social_media/settings.py

--- social_media/settings.py
+++ social_media/settings.py
@@ -76,16 +76,11 @@ WSGI_APPLICATION = 'social_media.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "test",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "127.0.0.1",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
